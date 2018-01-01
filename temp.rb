29f6f4d25cab355f119438fcdfa25c01fb8437d5
Alexander <odetocodeman@gmail.com>
2017-12-31 23:13:14 -0700

    Delete temp.rb

--- temp.rb
+++
@@ -1,5 +0,0 @@
-  #reduce dimensions of unit
-  #possible because time forms a line with 30 min blocks as ints and minutes and smaller frames of time expressed in decimal range
-  halfHours += 100 ** -1 * minutes
-  print 3
-  return halfHours
